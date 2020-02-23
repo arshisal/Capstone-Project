f34dbef85a890f4cd099ac6ee87810eb97b7156d
Sally A <33629611+arshisal@users.noreply.github.com>
2020-02-22 20:55:27 -0500

    Meteorological Data Prep

---
+++ Data Prep Meteo Data.R
@@ -0,0 +1,109 @@
+rm(list = ls())
+
+# Uploading raw data
+# 2006
+M1_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/01_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M2_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/02_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M3_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/03_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M4_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/04_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M5_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/05_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M6_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/06_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M7_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/07_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M8_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/08_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M9_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/09_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M10_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/10_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M11_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/11_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+M12_2006 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/12_2006.csv", stringsAsFactors = FALSE, header = TRUE)
+
+#2007
+M1_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/01_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M2_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/02_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M3_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/03_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M4_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/04_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M5_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/05_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M6_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/06_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M7_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/07_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M8_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/08_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M9_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/09_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M10_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/10_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M11_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/11_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+M12_2007 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/12_2007.csv", stringsAsFactors = FALSE, header = TRUE)
+
+#2008
+M1_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/01_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M2_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/02_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M3_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/03_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M4_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/04_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M5_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/05_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M6_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/06_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M7_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/07_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M8_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/08_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M9_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/09_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M10_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/10_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M11_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/11_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+M12_2008 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/12_2008.csv", stringsAsFactors = FALSE, header = TRUE)
+
+#2009
+M1_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/01_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M2_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/02_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M3_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/03_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M4_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/04_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M5_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/05_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M6_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/06_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M7_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/07_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M8_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/08_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M9_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/09_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M10_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/10_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M11_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/11_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+M12_2009 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/12_2009.csv", stringsAsFactors = FALSE, header = TRUE)
+
+#2010
+M1_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/01_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M2_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/02_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M3_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/03_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M4_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/04_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M5_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/05_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M6_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/06_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M7_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/07_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M8_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/08_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M9_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/09_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M10_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/10_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M11_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/11_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+M12_2010 <- read.csv("/Users/SallyA/Documents/Capstone CKME/Data Files/Weather/12_2010.csv", stringsAsFactors = FALSE, header = TRUE)
+
+# Combine the monthly data to build the annual datasets
+Climate_2006 <- rbind(M1_2006, M2_2006, M3_2006, M4_2006, M5_2006, M6_2006, M7_2006, M8_2006, M9_2006, M10_2006, M11_2006, M12_2006)
+Climate_2007 <- rbind(M1_2007, M2_2007, M3_2007, M4_2007, M5_2007, M6_2007, M7_2007, M8_2007, M9_2007, M10_2007, M11_2007, M12_2007)
+Climate_2008 <- rbind(M1_2008, M2_2008, M3_2008, M4_2008, M5_2008, M6_2008, M7_2008, M8_2008, M9_2008, M10_2008, M11_2008, M12_2008)
+Climate_2009 <- rbind(M1_2009, M2_2009, M3_2009, M4_2009, M5_2009, M6_2009, M7_2009, M8_2009, M9_2009, M10_2009, M11_2009, M12_2009)
+Climate_2010 <- rbind(M1_2010, M2_2010, M3_2010, M4_2010, M5_2010, M6_2010, M7_2010, M8_2010, M9_2010, M10_2010, M11_2010, M12_2010)
+
+# Cleaning Extra Data
+rm(M1_2006, M2_2006, M3_2006, M4_2006, M5_2006, M6_2006, M7_2006, M8_2006, M9_2006, M10_2006, M11_2006, M12_2006)
+rm(M1_2007, M2_2007, M3_2007, M4_2007, M5_2007, M6_2007, M7_2007, M8_2007, M9_2007, M10_2007, M11_2007, M12_2007)
+rm(M1_2008, M2_2008, M3_2008, M4_2008, M5_2008, M6_2008, M7_2008, M8_2008, M9_2008, M10_2008, M11_2008, M12_2008)
+rm(M1_2009, M2_2009, M3_2009, M4_2009, M5_2009, M6_2009, M7_2009, M8_2009, M9_2009, M10_2009, M11_2009, M12_2009)
+rm(M1_2010, M2_2010, M3_2010, M4_2010, M5_2010, M6_2010, M7_2010, M8_2010, M9_2010, M10_2010, M11_2010, M12_2010)
+
+# Retreiving feature columns from full dataset
+mod_2006 <- Climate_2006[,c("Date.Time", "Year", "Month", "Day", "Time", "Temp...C.", "Rel.Hum....", "Wind.Dir..10s.deg.", "Wind.Spd..km.h.")]
+mod_2007 <- Climate_2007[,c("Date.Time", "Year", "Month", "Day", "Time", "Temp...C.", "Rel.Hum....", "Wind.Dir..10s.deg.", "Wind.Spd..km.h.")]
+mod_2008 <- Climate_2008[,c("Date.Time", "Year", "Month", "Day", "Time", "Temp...C.", "Rel.Hum....", "Wind.Dir..10s.deg.", "Wind.Spd..km.h.")]
+mod_2009 <- Climate_2009[,c("Date.Time", "Year", "Month", "Day", "Time", "Temp...C.", "Rel.Hum....", "Wind.Dir..10s.deg.", "Wind.Spd..km.h.")]
+mod_2010 <- Climate_2010[,c("Date.Time", "Year", "Month", "Day", "Time", "Temp...C.", "Rel.Hum....", "Wind.Dir..10s.deg.", "Wind.Spd..km.h.")]
+
+# Change column names to proper names
+Newnames <- c("Full Date", "Year", "Month", "Day", "Time", "Temp", "Humidity", "Wind Dir", "Wind Spd")
+colnames(mod_2006) <- Newnames
+colnames(mod_2007) <- Newnames
+colnames(mod_2008) <- Newnames
+colnames(mod_2009) <- Newnames
+colnames(mod_2010) <- Newnames
+
+# Summary Stat of the Data
+summary(mod_2006)
+summary(mod_2007)
+summary(mod_2008)
+summary(mod_2009)
+summary(mod_2010)
+
